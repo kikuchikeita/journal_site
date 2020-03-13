ef46283811ac43cdaa5e412f494c0cd100337583
kikuchikeita <imnotpooh@gmail.com>
2020-03-13 22:21:50 +0900

    論文アプリ修正しました

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,9 +15,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      redirect_to @article
-    else
-      render :new
+      redirect_to articles_path
     end
   end
 
